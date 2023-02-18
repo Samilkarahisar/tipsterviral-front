3ad54d596237301642cad4a5df4bfc0ad32bf447
YOROTOR\yohan <yohan.malbranque@gmail.com>
2023-02-18 18:36:16 +0100

    select image room and picture

--- src/res/icons.ts
+++ src/res/icons.ts
@@ -1,15 +1,27 @@
+import IconBathroom from '@/assets/icons/bathroom.png';
+import IconBedroom from '@/assets/icons/bedroom.png';
 import IconCheck from '@/assets/icons/check.svg';
 import IconClose from '@/assets/icons/close-button.svg';
+import IconCloset from '@/assets/icons/closet.png';
 import IconCode from '@/assets/icons/code.svg';
 import IconCustomization from '@/assets/icons/customization.svg';
 import IconDiscord from '@/assets/icons/discord.svg';
 import IconEmail from '@/assets/icons/email.svg';
 import IconFallbackImage from '@/assets/icons/fallback-image.svg';
+import IconGarage from '@/assets/icons/garage.png';
 import IconGoogle from '@/assets/icons/google.svg';
+import IconKitchen from '@/assets/icons/kitchen.png';
+import IconLivingRoom from '@/assets/icons/living-room.png';
 import IconMenu from '@/assets/icons/menu.svg';
 import IconOptions from '@/assets/icons/options.svg';
 import IconTitle from '@/assets/icons/title.svg';
+import IconToilet from '@/assets/icons/toilet.png';
 import IconTwitter from '@/assets/icons/twitter.svg';
+import IconBricks from '@/assets/images/bricks.png';
+import IconGreen from '@/assets/images/green.png';
+import IconLux from '@/assets/images/lux.png';
+import IconMarble from '@/assets/images/marble.png';
+import IconWood from '@/assets/images/wood.png';
 
 export {
   IconCheck,
@@ -24,4 +36,16 @@ export {
   IconDiscord,
   IconTitle,
   IconTwitter,
+  IconGarage,
+  IconKitchen,
+  IconLivingRoom,
+  IconToilet,
+  IconBathroom,
+  IconBedroom,
+  IconCloset,
+  IconWood,
+  IconGreen,
+  IconLux,
+  IconBricks,
+  IconMarble,
 };
