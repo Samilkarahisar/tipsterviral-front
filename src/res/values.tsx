b55838c9fd6ce40d1b8a4a36dd74b288b5199c6c
Samil <samilkarahisar@gmail.com>
2023-03-13 07:53:27 +0100

    reduce styles for now

--- src/res/values.tsx
+++ src/res/values.tsx
@@ -54,61 +54,14 @@ export const styleList = [
   },
   {
     image:
-      'https://cdn.pixabay.com/photo/2020/06/07/01/50/window-5268702_1280.jpg',
-    label: 'Minimalist',
-    value: '3',
-  },
-  {
-    image:
-      'https://cdn.pixabay.com/photo/2019/05/29/19/51/house-4238414_1280.jpg',
-    label: 'Scandinavian',
-    value: '4',
-  },
-  {
-    image:
-      'https://cdn.pixabay.com/photo/2018/03/12/20/07/maldives-3220702_1280.jpg',
-    label: 'Tropical',
-    value: '5',
-  },
-  {
-    image:
       'https://cdn.pixabay.com/photo/2015/06/27/16/34/wall-823611_1280.jpg',
     label: 'Industrial',
-    value: '2',
-  },
-  {
-    image:
-      'https://cdn.pixabay.com/photo/2021/03/02/01/07/cyberpunk-6061251_1280.jpg',
-    label: 'Cyberpunk',
     value: '3',
-  },
-  {
-    image:
-      'https://cdn.pixabay.com/photo/2019/11/04/19/15/steampunk-4601917_1280.jpg',
-    label: 'Steampunk',
-    value: '4',
-  },
-  {
-    image: 'https://cdn.pixabay.com/photo/2015/04/08/13/22/car-712684_1280.jpg',
-    label: 'Vice City',
-    value: '5',
   },
   {
     image:
       'https://cdn.pixabay.com/photo/2014/12/06/12/47/fireplace-558985_1280.jpg',
     label: 'Christmas',
-    value: '5',
-  },
-  {
-    image:
-      'https://cdn.pixabay.com/photo/2014/10/15/01/57/catherines-palace-489085_1280.jpg',
-    label: 'Palace',
-    value: '5',
-  },
-  {
-    image:
-      'https://cdn.pixabay.com/photo/2018/07/13/09/04/architecture-3535243_1280.jpg',
-    label: 'Morocco',
-    value: '5',
+    value: '4',
   },
 ];
