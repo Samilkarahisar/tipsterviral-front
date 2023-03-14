494f566159545ecc47381c1e9447173e7872ab24
Samil <samilkarahisar@gmail.com>
2023-03-14 14:14:26 +0100

    changes in landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
+import { FieldTimeOutlined } from '@ant-design/icons';
 import { Col, Row } from 'antd';
 import { NextSeo } from 'next-seo';
 import router from 'next/router';
 import React from 'react';
-
 const HomePage = () => {
   return (
     <>
@@ -18,9 +18,9 @@ const HomePage = () => {
       </Row>
       <Row className="font-Alexandria lg:text-xl text-xl max-w-[1200px] mx-auto px-10 laptop:px-20 items-center">
         <Col className="pr-14">
-          <div>Importez une photo vide de votre pièce</div>
-          <div>Choisissez un style</div>
-          <div>Obtenez une version meublée</div>
+          <div>1. Importez une photo vide de votre pièce</div>
+          <div>2. Choisissez un style</div>
+          <div>3. Obtenez une version meublée</div>
         </Col>
         <Col className="mt-4 laptop:pl-6 ">
           <button
@@ -37,16 +37,23 @@ const HomePage = () => {
           }
           className="object-contain rounded-[25px] small:h-auto small:w-auto min-h-[200px] min-w-[200px] laptop:w-[320px] laptop:h-[320px]"
         />
-        <div className="relative">
-          <div className="small:h-auto small:w-auto min-h-[200px] min-w-[200px] laptop:w-[75px] laptop:h-[75px]"></div>
-          <div className="absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <div className="relative">
-              <div className="rounded-full overflow-hidden h-24 w-72 flex items-center justify-center bg-transparent border-0 rotate-90 laptop:rotate-0">
-                <img
-                  src="https://i.ibb.co/b5kd9b3/Nouveau-projet-23.png"
-                  alt="Image"
-                  className="h-12 w-36"
-                />
+
+        <div className="flex flex-col items-center">
+          <FieldTimeOutlined className="!text-[#ef8b34] text-4xl pt-10 laptop:mt-16" />
+          <div className="flex items-center text-l text-bold text-[#ef8b34] ">
+            60 secondes
+          </div>
+          <div className="relative">
+            <div className="small:h-auto small:w-auto min-h-[200px] laptop:min-h-[0px] min-w-[200px] laptop:w-[75px] laptop:h-[75px]"></div>
+            <div className="absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <div className="relative">
+                <div className="rounded-full overflow-hidden h-24 w-72 flex items-center justify-center bg-transparent border-0 rotate-90 laptop:rotate-0">
+                  <img
+                    src="https://i.ibb.co/b5kd9b3/Nouveau-projet-23.png"
+                    alt="Image"
+                    className="h-12 w-36"
+                  />
+                </div>
               </div>
             </div>
           </div>
