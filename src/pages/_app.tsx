f220602472cd9813696cfe2833de4586c4aeb981
samil <samilkarahisar@gmail.com>
2023-03-29 11:46:56 +0200

    add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,13 @@ import '@/styles/globals.css';
 import 'antd/dist/antd.css';
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { hotjar } from 'react-hotjar';
 import Wrapper from '../components/ui/Wrapper';
-
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(3428804, 6);
+  }, []);
   return (
     <>
       <DefaultSeo
